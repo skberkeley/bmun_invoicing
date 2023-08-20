a9e6e4a87757741d4837225e3488ce300885b198
Sora Kanosue <sorakanosue@berkeley.edu>
2023-08-21 00:23:53 +0200

    Added some utility functions for use in getOrCreateInvoicesFromRegistration

---
+++ app/src/main/java/invoice_automation/Util.java
@@ -0,0 +1,64 @@
+package invoice_automation;
+
+import com.intuit.ipp.data.Customer;
+import com.intuit.ipp.data.Invoice;
+import com.intuit.ipp.data.ItemLineDetail;
+import com.intuit.ipp.data.Line;
+import com.intuit.ipp.data.LineDetailTypeEnum;
+import com.intuit.ipp.data.ReferenceType;
+import invoice_automation.model.InvoiceType;
+import invoice_automation.model.Registration;
+import lombok.NonNull;
+
+import java.util.Objects;
+
+public class Util {
+    /**
+     * Utility method to check that an Invoice corresponds to the passed Customer. If the Invoice has no CustomerRef,
+     * returns false, else checks that the Customer Ref's value matches the Customer's id.
+     * @param invoice - The Invoice to check
+     * @param customer - The Customer to compare against
+     * @return - Whether the Invoice corresponds to the passed Customer
+     */
+    public static boolean checkInvoiceMatchesCustomer(@NonNull Invoice invoice, @NonNull Customer customer) {
+        ReferenceType customerRef = invoice.getCustomerRef();
+
+        if (customerRef == null) {
+            return false;
+        }
+
+        return customerRef.getValue().equals(customer.getId());
+    }
+
+    /**
+     * Gets the InvoiceType from the Invoice, based on the Invoice's line items. Expects at least one line to be of type
+     * Sales Item, and expects the Invoice to have at most one Line item corresponding to an Invoice Type, based on the
+     * Line Item's name. Returns null if none match, or there are no line items of correct type.
+     * @param invoice
+     * @return
+     */
+    public static InvoiceType getInvoiceTypeFromInvoice(@NonNull Invoice invoice) {
+        return invoice.getLine()
+                .stream()
+                .filter(line -> line.getDetailType() == LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL)
+                .map(Line::getSalesItemLineDetail)
+                .map(ItemLineDetail::getItemRef)
+                .map(ReferenceType::getName)
+                .map(Util::getInvoiceTypeFromLineItemName)
+                .filter(Objects::nonNull)
+                .findAny().orElse(null);
+    }
+
+    private static InvoiceType getInvoiceTypeFromLineItemName(@NonNull String lineItemName) {
+        if (lineItemName.equals(InvoiceType.BMUN_SCHOOL_FEE.toString())) {
+            return InvoiceType.BMUN_SCHOOL_FEE;
+        } else if (lineItemName.equals(InvoiceType.BMUN_DELEGATE_FEE.toString())) {
+            return InvoiceType.BMUN_DELEGATE_FEE;
+        } else if (lineItemName.equals(InvoiceType.FC_SCHOOL_FEE.toString())) {
+            return InvoiceType.FC_SCHOOL_FEE;
+        } else if (lineItemName.equals(InvoiceType.FC_DELEGATE_FEE.toString())) {
+            return InvoiceType.FC_DELEGATE_FEE;
+        }
+        return null;
+    }
+}
---
+++ app/src/test/java/invoice_automation/UtilTest.java
@@ -0,0 +1,154 @@
+package invoice_automation;
+
+import com.intuit.ipp.data.Customer;
+import com.intuit.ipp.data.Invoice;
+import com.intuit.ipp.data.Line;
+import com.intuit.ipp.data.LineDetailTypeEnum;
+import com.intuit.ipp.data.ReferenceType;
+import com.intuit.ipp.data.SalesItemLineDetail;
+import invoice_automation.model.Address;
+import invoice_automation.model.InvoiceType;
+import invoice_automation.model.PaymentMethod;
+import invoice_automation.model.Registration;
+import invoice_automation.model.School;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+public class UtilTest {
+    @Mock
+    Invoice invoice;
+    @Mock
+    Customer customer;
+    @Mock
+    ReferenceType customerRef;
+    @Mock
+    Line line1;
+    @Mock
+    Line line2;
+    @Mock
+    SalesItemLineDetail salesItemLineDetail;
+    @Mock
+    ReferenceType itemRef;
+
+    private final Address address = new Address(
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+    );
+    private final School school = School.builder()
+            .schoolName("Cal")
+            .email("cal@cal.edu")
+            .phoneNumbers(List.of())
+            .address(address)
+            .build();
+    private final Registration.RegistrationBuilder registrationBuilder = Registration.builder()
+            .school(school)
+            .numDelegates(0)
+            .registrationDate(LocalDate.now())
+            .paymentMethod(PaymentMethod.CARD);
+    private final String CUSTOMER_ID = "ID";
+    private final String CUSTOMER_ID1 = "id";
+
+    @Test
+    public void testCheckInvoiceMatchesCustomer_happyPath_invoiceAndCustomerMatch() {
+        // Setup
+        when(invoice.getCustomerRef()).thenReturn(customerRef);
+        when(customerRef.getValue()).thenReturn(CUSTOMER_ID);
+        when(customer.getId()).thenReturn(CUSTOMER_ID);
+
+        // Do
+        boolean match = Util.checkInvoiceMatchesCustomer(invoice, customer);
+
+        // Check
+        assertTrue(match);
+    }
+
+    @Test
+    public void testCheckInvoiceMatchesCustomer_happyPath_invoiceAndCustomerNoMatch() {
+        // Setup
+        when(invoice.getCustomerRef()).thenReturn(customerRef);
+        when(customerRef.getValue()).thenReturn(CUSTOMER_ID);
+        when(customer.getId()).thenReturn(CUSTOMER_ID1);
+
+        // Do
+        boolean match = Util.checkInvoiceMatchesCustomer(invoice, customer);
+
+        // Check
+        assertFalse(match);
+    }
+
+    @Test
+    public void testCheckInvoiceMatchesCustomer_happyPath_invoiceCustomerRefIsNull() {
+        // Setup
+        when(invoice.getCustomerRef()).thenReturn(null);
+
+        // Do
+        boolean match = Util.checkInvoiceMatchesCustomer(invoice, customer);
+
+        // Check
+        assertFalse(match);
+    }
+
+    @Test
+    public void testGetInvoiceTypeFromInvoice_happyPath() {
+        // Setup
+        when(invoice.getLine()).thenReturn(List.of(line1, line2));
+        when(line1.getDetailType()).thenReturn(LineDetailTypeEnum.TAX_LINE_DETAIL);
+        when(line2.getDetailType()).thenReturn(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+        when(line2.getSalesItemLineDetail()).thenReturn(salesItemLineDetail);
+        when(salesItemLineDetail.getItemRef()).thenReturn(itemRef);
+
+        // Do, Check
+        when(itemRef.getName()).thenReturn(InvoiceType.BMUN_SCHOOL_FEE.toString());
+        assertEquals(InvoiceType.BMUN_SCHOOL_FEE, Util.getInvoiceTypeFromInvoice(invoice));
+
+        when(itemRef.getName()).thenReturn(InvoiceType.BMUN_DELEGATE_FEE.toString());
+        assertEquals(InvoiceType.BMUN_DELEGATE_FEE, Util.getInvoiceTypeFromInvoice(invoice));
+
+        when(itemRef.getName()).thenReturn(InvoiceType.FC_SCHOOL_FEE.toString());
+        assertEquals(InvoiceType.FC_SCHOOL_FEE, Util.getInvoiceTypeFromInvoice(invoice));
+
+        when(itemRef.getName()).thenReturn(InvoiceType.FC_DELEGATE_FEE.toString());
+        assertEquals(InvoiceType.FC_DELEGATE_FEE, Util.getInvoiceTypeFromInvoice(invoice));
+    }
+
+    @Test
+    public void testGetInvoiceTypeFromInvoice_noSalesLineItemMatches() {
+        // Setup
+        when(invoice.getLine()).thenReturn(List.of(line1, line2));
+        when(line1.getDetailType()).thenReturn(LineDetailTypeEnum.TAX_LINE_DETAIL);
+        when(line2.getDetailType()).thenReturn(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+        when(line2.getSalesItemLineDetail()).thenReturn(salesItemLineDetail);
+        when(salesItemLineDetail.getItemRef()).thenReturn(itemRef);
+        when(itemRef.getName()).thenReturn("UCBMUN");
+
+        // Do, Check
+        assertNull(Util.getInvoiceTypeFromInvoice(invoice));
+    }
+
+    @Test
+    public void testGetInvoiceTypeFromInvoice_noSalesLineItems() {
+        // Setup
+        when(invoice.getLine()).thenReturn(List.of(line1, line2));
+        when(line1.getDetailType()).thenReturn(LineDetailTypeEnum.TAX_LINE_DETAIL);
+        when(line2.getDetailType()).thenReturn(LineDetailTypeEnum.TAX_LINE_DETAIL);
+
+        // Do, Check
+        assertNull(Util.getInvoiceTypeFromInvoice(invoice));
+    }
+}
