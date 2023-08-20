97c757067a65d5dbbfc4df3c1eaccb52b3c3de66
Sora Kanosue <sorakanosue@berkeley.edu>
2023-08-21 00:21:32 +0200

    Added some toString methods to Conference and InvoiceType for use in utility functions later

--- app/src/main/java/invoice_automation/Consts.java
+++ app/src/main/java/invoice_automation/Consts.java
@@ -2,8 +2,11 @@ package invoice_automation;
 
 public class Consts {
     public static final int BMUN_SESSION_NO = 72;
+    public static final int FC_NO = 3;
     /**
      * The base URL to use for API calls when in Sandbox mode
      */
     public static final String SANDBOX_BASE_URL = "https://sandbox-quickbooks.api.intuit.com/v3/company";
+    public static final String SCHOOL_FEE = "School Fee";
+    public static final String DELEGATE_FEE = "Delegate Fee";
 }
