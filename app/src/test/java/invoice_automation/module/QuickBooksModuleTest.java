c9d83722355548a407e541228148296041480576
Sora Kanosue <sorakanosue@berkeley.edu>
2023-09-05 21:47:02 -0700

    Implemented createInvoicesFromRegistration in QuickBooksModule.java

--- app/src/test/java/invoice_automation/module/QuickBooksModuleTest.java
+++ app/src/test/java/invoice_automation/module/QuickBooksModuleTest.java
@@ -5,7 +5,12 @@ import com.intuit.ipp.data.Customer;
 import com.intuit.ipp.data.EmailAddress;
 import com.intuit.ipp.data.EmailStatusEnum;
 import com.intuit.ipp.data.Invoice;
+import com.intuit.ipp.data.Item;
+import com.intuit.ipp.data.Line;
+import com.intuit.ipp.data.LineDetailTypeEnum;
 import com.intuit.ipp.data.MemoRef;
+import com.intuit.ipp.data.ReferenceType;
+import com.intuit.ipp.data.SalesItemLineDetail;
 import com.intuit.ipp.exception.FMSException;
 import com.intuit.ipp.services.DataService;
 import com.intuit.ipp.util.Config;
@@ -14,8 +19,10 @@ import invoice_automation.Util;
 import invoice_automation.model.Address;
 import invoice_automation.model.Conference;
 import invoice_automation.model.InvoiceType;
+import invoice_automation.model.ItemType;
 import invoice_automation.model.PaymentMethod;
 import invoice_automation.model.Registration;
+import invoice_automation.model.RegistrationRound;
 import invoice_automation.model.School;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,13 +31,17 @@ import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import static invoice_automation.Consts.SANDBOX_BASE_URL;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
@@ -45,6 +56,11 @@ public class QuickBooksModuleTest {
     private static final String EMAIL_ADDRESS = "oski@berkeley.edu";
     private static final String MEMO = "memo123";
     private static final String SCHOOL_NAME = "Cal";
+    private static final String CUSTOMER_ID = "customer id";
+    private static final String FC_SCHOOL_FEE_ITEM_ID = "fc school fee item id";
+    private static final String FC_DEL_FEE_ITEM_ID = "fc del fee item id";
+    private static final String CC_FEE_ITEM_ID = "credit card processing fee id";
+    private static final int NUM_DELEGATES = 10;
 
     @Mock
     private DataService dataService;
@@ -68,12 +84,13 @@ public class QuickBooksModuleTest {
             .phoneNumbers(List.of())
             .address(address)
             .build();
+    private final LocalDate registrationDate = LocalDate.of(2023, 9, 4);
     private final Registration registration = Registration.builder()
             .school(school)
-            .numDelegates(0)
-            .registrationDate(LocalDate.now())
+            .numDelegates(NUM_DELEGATES)
+            .registrationDate(registrationDate)
             .paymentMethod(PaymentMethod.CARD)
-            .conference(Conference.BMUN)
+            .conference(Conference.FC)
             .build();
 
     @Test
@@ -290,6 +307,161 @@ public class QuickBooksModuleTest {
 
         // Verify
         assertEquals(Map.of(), invoiceMap);
+    }
+
+    private ReferenceType getExpectedCustomerRef_for_createInvoicesFromRegistration() {
+        ReferenceType expectedCustomerRef = new ReferenceType();
+        expectedCustomerRef.setValue(CUSTOMER_ID);
+        expectedCustomerRef.setName(SCHOOL_NAME);
+        return expectedCustomerRef;
+    }
+
+    private Date getExpectedTxnDate_for_createInvoicesFromRegistration() {
+        return java.sql.Date.valueOf(registrationDate);
+    }
+
+    private boolean getAllowOnlineCreditCardPayment_for_createInvoicesFromRegistration() {
+        return registration.getPaymentMethod() == PaymentMethod.CARD;
+    }
+
+    private ReferenceType getFCSchoolFeeItemRef() {
+        ReferenceType itemRef = new ReferenceType();
+        itemRef.setValue(FC_SCHOOL_FEE_ITEM_ID);
+        itemRef.setName(ItemType.FC_SCHOOL_FEE.toString());
+        return itemRef;
+    }
+
+    private ReferenceType getFCDelFeeItemRef() {
+        ReferenceType itemRef = new ReferenceType();
+        itemRef.setValue(FC_DEL_FEE_ITEM_ID);
+        itemRef.setName(ItemType.FC_DELEGATE_FEE.toString());
+        return itemRef;
+    }
+
+    private ReferenceType getCCFeeItemRef() {
+        ReferenceType itemRef = new ReferenceType();
+        itemRef.setValue(CC_FEE_ITEM_ID);
+        itemRef.setName(ItemType.CREDIT_CARD_PROCESSING_FEE.toString());
+        return itemRef;
+    }
+
+    private BigDecimal computeCCFeeAmount(BigDecimal subtotal) {
+        BigDecimal fee = BigDecimal.valueOf(0.029).multiply(subtotal);
+        fee = fee.add(BigDecimal.valueOf(0.25));
+        fee = fee.divide(BigDecimal.valueOf(0.971), 2, RoundingMode.CEILING);
+        return fee;
+    }
+
+    private Line getCCFeeLineItem(BigDecimal amount) {
+        Line lineItem = new Line();
+        lineItem.setDetailType(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+        lineItem.setAmount(amount);
+
+        SalesItemLineDetail salesItemLineDetail = new SalesItemLineDetail();
+        salesItemLineDetail.setItemRef(getCCFeeItemRef());
+        salesItemLineDetail.setQty(BigDecimal.ONE);
+        salesItemLineDetail.setUnitPrice(amount);
+        lineItem.setSalesItemLineDetail(salesItemLineDetail);
+
+        return lineItem;
+    }
+
+    private Invoice getExpectedSchoolFeeInvoice_for_createInvoicesFromRegistration() {
+        Invoice invoice = new Invoice();
+        // Fill in invoice details
+        ReferenceType expectedCustomerRef = getExpectedCustomerRef_for_createInvoicesFromRegistration();
+        invoice.setCustomerRef(expectedCustomerRef);
+        invoice.setTxnDate(getExpectedTxnDate_for_createInvoicesFromRegistration());
+        invoice.setDueDate(java.sql.Date.valueOf(RegistrationRound.FC_ROUND_1.getSchoolFeeDueDate()));
+        invoice.setAllowOnlineCreditCardPayment(getAllowOnlineCreditCardPayment_for_createInvoicesFromRegistration());
+        // Create school fee line item
+        Line schoolFeeLineItem = new Line();
+        schoolFeeLineItem.setDetailType(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+        schoolFeeLineItem.setAmount(InvoiceType.FC_SCHOOL_FEE.getUnitCost());
+
+        SalesItemLineDetail schoolFeeSalesItemLineDetail = new SalesItemLineDetail();
+        schoolFeeSalesItemLineDetail.setItemRef(getFCSchoolFeeItemRef());
+        schoolFeeSalesItemLineDetail.setQty(BigDecimal.ONE);
+        schoolFeeSalesItemLineDetail.setUnitPrice(InvoiceType.FC_SCHOOL_FEE.getUnitCost());
+        schoolFeeLineItem.setSalesItemLineDetail(schoolFeeSalesItemLineDetail);
+        // Get CC fee line item
+        Line ccLineItem = getCCFeeLineItem(computeCCFeeAmount(InvoiceType.FC_SCHOOL_FEE.getUnitCost()));
+
+        // Set lines
+        invoice.setLine(List.of(schoolFeeLineItem, ccLineItem));
+
+        return invoice;
+    }
+
+    private Invoice getExpectedDelegateFeeInvoice_for_createInvoicesFromRegistration() {
+        Invoice invoice = new Invoice();
+        // Fill in invoice details
+        ReferenceType expectedCustomerRef = getExpectedCustomerRef_for_createInvoicesFromRegistration();
+        invoice.setCustomerRef(expectedCustomerRef);
+        invoice.setTxnDate(getExpectedTxnDate_for_createInvoicesFromRegistration());
+        invoice.setDueDate(java.sql.Date.valueOf(RegistrationRound.FC_ROUND_1.getDelegateFeeDueDate()));
+        invoice.setAllowOnlineCreditCardPayment(getAllowOnlineCreditCardPayment_for_createInvoicesFromRegistration());
+        // Create del fee line item
+        Line delFeeLineItem = new Line();
+        delFeeLineItem.setDetailType(LineDetailTypeEnum.SALES_ITEM_LINE_DETAIL);
+        BigDecimal subtotal = InvoiceType.FC_DELEGATE_FEE.getUnitCost().multiply(new BigDecimal(NUM_DELEGATES));
+        delFeeLineItem.setAmount(subtotal);
+
+        SalesItemLineDetail delFeeSalesItemLineDetail = new SalesItemLineDetail();
+        delFeeSalesItemLineDetail.setItemRef(getFCDelFeeItemRef());
+        delFeeSalesItemLineDetail.setQty(new BigDecimal(NUM_DELEGATES));
+        delFeeSalesItemLineDetail.setUnitPrice(InvoiceType.FC_DELEGATE_FEE.getUnitCost());
+        delFeeLineItem.setSalesItemLineDetail(delFeeSalesItemLineDetail);
+        // Get CC fee line item
+        Line ccLineItem = getCCFeeLineItem(computeCCFeeAmount(subtotal));
+
+        // Set lines
+        invoice.setLine(List.of(delFeeLineItem, ccLineItem));
+
+        return invoice;
+    }
+
+    @Test
+    public void testCreateInvoicesFromRegistration_happyPath() throws Exception {
+        // Setup
+        setupMethodTests();
+        // Mock customer to be used in CustomerRef
+        when(dataService.findAll(any(Customer.class))).thenReturn(List.of(customer));
+        when(customer.getId()).thenReturn(CUSTOMER_ID);
+        when(customer.getDisplayName()).thenReturn(SCHOOL_NAME);
+        // Mock items to be added to invoices
+        Item fcSchoolFeeItem = new Item();
+        fcSchoolFeeItem.setId(FC_SCHOOL_FEE_ITEM_ID);
+        fcSchoolFeeItem.setName(ItemType.FC_SCHOOL_FEE.toString());
+        Item fcDelFeeItem = new Item();
+        fcDelFeeItem.setId(FC_DEL_FEE_ITEM_ID);
+        fcDelFeeItem.setName(ItemType.FC_DELEGATE_FEE.toString());
+        Item ccFeeItem = new Item();
+        ccFeeItem.setId(CC_FEE_ITEM_ID);
+        ccFeeItem.setName(ItemType.CREDIT_CARD_PROCESSING_FEE.toString());
+        when(dataService.findAll(any(Item.class))).thenReturn(List.of(fcSchoolFeeItem, fcDelFeeItem, ccFeeItem));
+
+
+        // Run
+        Map<InvoiceType, Invoice> invoiceMap = quickBooksModule.createInvoicesFromRegistration(registration);
+
+        // Verify
+        // Construct expected invoices
+        Invoice expectedDelegateFeeInvoice = getExpectedDelegateFeeInvoice_for_createInvoicesFromRegistration();
+        Invoice expectedSchoolFeeInvoice = getExpectedSchoolFeeInvoice_for_createInvoicesFromRegistration();
+
+        assertTrue(invoiceMap.containsKey(InvoiceType.FC_SCHOOL_FEE));
+        Invoice actualSchoolFeeInvoice = invoiceMap.get(InvoiceType.FC_SCHOOL_FEE);
+        assertTrue(expectedSchoolFeeInvoice.equals(invoiceMap.get(InvoiceType.FC_SCHOOL_FEE)));
+        assertEquals(expectedSchoolFeeInvoice.getLine(), actualSchoolFeeInvoice.getLine());
+
+        assertTrue(invoiceMap.containsKey(InvoiceType.FC_DELEGATE_FEE));
+        Invoice actualDelegateFeeInvoice = invoiceMap.get(InvoiceType.FC_DELEGATE_FEE);
+        boolean delFeeInvoiceMatches = expectedDelegateFeeInvoice.equals(actualDelegateFeeInvoice);
+        assertTrue(delFeeInvoiceMatches);
+        assertEquals(expectedDelegateFeeInvoice.getLine(), actualDelegateFeeInvoice.getLine());
+        Mockito.verify(dataService).add(expectedDelegateFeeInvoice);
+        Mockito.verify(dataService).add(expectedSchoolFeeInvoice);
     }
 }
 
