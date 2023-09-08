84f0a9c6d10ea0d08a9253ce8dadecfaf3081b25
Sora Kanosue <66887530+skberkeley@users.noreply.github.com>
2023-09-07 21:41:37 -0700

    Implement RegistrationHandler + manual test in App.java (#9)

--- app/src/main/java/invoice_automation/handler/RegistrationHandler.java
+++ app/src/main/java/invoice_automation/handler/RegistrationHandler.java
@@ -1,9 +1,15 @@
 package invoice_automation.handler;
 
+import com.intuit.ipp.data.Customer;
+import com.intuit.ipp.data.EmailStatusEnum;
+import com.intuit.ipp.data.Invoice;
+import invoice_automation.model.InvoiceType;
 import invoice_automation.module.QuickBooksModule;
 import invoice_automation.model.Registration;
 import lombok.Builder;
 import lombok.NonNull;
+
+import java.util.Map;
 
 /**
  * A class to handle new registrations
@@ -28,8 +34,27 @@ public class RegistrationHandler {
         this.quickBooksModule = new QuickBooksModule(accessToken, realmId, useSandbox);
     }
 
+    /**
+     * Carry out the actions needed to handle a registration:
+     *  - Create or get the associated QuickBooks customer
+     *  - Query whether any matching invoices exist
+     *  - If invoices don't exist, create them
+     *  - Send each invoice if it hasn't been sent
+     * @param registration
+     */
     public void handleRegistration(@NonNull Registration registration) {
-        // TODO: IA-3
-        return;
+        Customer customer = this.quickBooksModule.updateCustomerFromSchool(registration.getSchool());
+
+        Map<InvoiceType, Invoice> invoices = this.quickBooksModule.queryInvoicesFromRegistration(registration);
+
+        if (invoices.isEmpty()) {
+            invoices = this.quickBooksModule.createInvoicesFromRegistration(registration);
+        }
+
+        for (Invoice invoice : invoices.values()) {
+            if (invoice.getEmailStatus() != EmailStatusEnum.EMAIL_SENT) {
+                this.quickBooksModule.sendInvoice(invoice, customer.getPrimaryEmailAddr().getAddress());
+            }
+        }
     }
 }
